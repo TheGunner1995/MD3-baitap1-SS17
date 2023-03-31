84bdc782b81d7244415e85f37199257c412b8915
Tô Việt Hoàng <totranhieu@gmail.com>
2023-03-31 15:28:00 +0700

    aaa

---
+++ src/Product.java
@@ -0,0 +1,57 @@
+import java.io.Serializable;
+
+public class Product implements Serializable {
+    private int id;
+    private String name;
+    private String manufacturer;
+    private int Price;
+    public Product() {
+    }
+    public Product(int id, String name, String manufacturer, int Price) {
+        this.id = id;
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.Price = Price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getPrice() {
+        return Price;
+    }
+
+    public void setPrice(int price) {
+        Price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", manufacturer='" + manufacturer + '\'' +
+                '}';
+    }
+}
